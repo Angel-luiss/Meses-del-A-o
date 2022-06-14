427a141c432868a6bc5f2f5bbd8395a7da68b11b
Angel <Angel>
2022-06-14 01:58:03 -0600

    Initial Commit

---
+++ meses del año/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  meses del año
+//
+//  Created by Angel Tucubal on 14/06/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
